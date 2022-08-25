47b429269ff3cea65a71bada1355dfa4d409d401
RCayre <rcayre@laas.fr>
2022-08-25 23:00:00 +0200

    Adding automatic installation of devices permissions

--- setup.py
+++ setup.py
@@ -1,4 +1,94 @@
 from setuptools import setup
+from setuptools.command.install import install
+from sys import platform, exit, executable
+from subprocess import Popen, DEVNULL, PIPE
+from os import listdir, geteuid
+from os.path import exists, realpath
+from shutil import copy
 
+class DevicesInstall(install):
+    UDEV_LOCATION = [
+        "/etc/udev/rules.d",
+        "/lib/udev/rules.d",
+        "/run/udev/rules.d",
+        "/var/run/udev/rules.d"
+    ]
+
+    def install_udev_rule(self, name):
+        print("Installing rules for %s device ..." % name)
+        matching_rules = [udev_name for udev_name in self.udev_rules if name in udev_name]
+        if len(matching_rules) == 0:
+            if exists("rules/"+name+".rules"):
+                copy("rules/"+name+".rules", "/usr/lib/udev/rules.d/40-"+name+".rules")
+                return True
+            else:
+                print("Rule not found, skipping.")
+                return False
+        else:
+            print("Rules detected for device %s (%s), skipping." % (name, ",".join(matching_rules)))
+            return False
+
+    def reload_udev_rules(self):
+        print("Reloading udev rules...")
+        process = Popen(['udevadm', 'control', '--reload-rules'], stderr=PIPE, stdout=PIPE)
+        stdout, stderr = process.communicate()
+        if len(stderr) > 0:
+            return False
+
+        process = Popen(['udevadm', 'trigger'],  stderr=PIPE, stdout=PIPE)
+        stdout, stderr = process.communicate()
+        if len(stderr) > 0:
+            return False
+
+        return True
+
+    def install_hci_capabilities(self, interpreter):
+        print("Installing HCI capabilities...")
+        process = Popen(['getcap', interpreter], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
+
+        if "cap_net_admin,cap_net_raw=eip" not in stdout.decode('utf-8'):
+            process = Popen(['setcap', 'cap_net_admin,cap_net_raw+eip',interpreter], stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process.communicate()
+
+            if len(stdout) != 0 or len(stderr) != 0:
+                return False
+            return True
+        else:
+            return True
+
+    def run(self):
+        if platform == "linux" or platform == "linux2":
+            if geteuid() != 0:
+                print("You should run this installer as root !")
+                exit(1)
+
+            print("Installing devices...")
+            # linux
+            self.udev_rules = []
+            for location in DevicesInstall.UDEV_LOCATION:
+                if exists(location):
+                    self.udev_rules += listdir(location)
+
+            self.install_udev_rule("ubertooth")
+            self.install_udev_rule("rzusbstick")
+
+            if not self.reload_udev_rules():
+                print("An error occured during udev rules reload.")
+
+            python_interpreter = realpath(executable)
+            if not self.install_hci_capabilities(python_interpreter):
+                print("An error occured during HCI capabilities installation.")
+
+
+        elif platform == "darwin":
+            # OS X
+            print("Automatic device installation is not supported on Mac OS/X.")
+            exit(1)
+        elif platform == "win32":
+            # Windows
+            print("Automatic device installation is not supported on Windows.")
+            exit(1)
+            
 if __name__ == "__main__":
     setup()
