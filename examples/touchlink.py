90601aef15216dac3edbdb479d16ee363c3b9c4a
RCayre <rcayre@laas.fr>
2022-12-13 00:20:37 +0100

    removing buffy code

--- examples/touchlink.py
+++ examples/touchlink.py
@@ -13,8 +13,4 @@ scan_response = Dot15d4(bytes.fromhex("21cc33ffff2a9d20feff570b00b76a2c1a11feff5
 network_router_join_request = Dot15d4(bytes.fromhex("21ccb7ffff2c1a11feff570b00f4ec2a9d20feff570b000b000300105ec011e3123f10aa2ae33a7768bac3a2780494a6708173d44fedc2110e92030c023d0019f4ec1800000000000000000000000000"))
 
 mgr = TouchlinkKeyManager(unencrypted_key=b"\xac\xbe\xf1Dp'\xd8\xd9Z\xfaB\xb0w\xe4\x88\xa5", transaction_id=0xea9cd138, response_id=0x8f8dbab4, key_index=0)
-#mgr.process_packet(scan_request)
-#mgr.process_packet(scan_response)
-#mgr.process_packet(network_router_join_request)
-#print(mgr.unencrypted_key)
 print(mgr.encrypted_key)
