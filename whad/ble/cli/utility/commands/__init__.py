4733b68caaf9111b76ead79aba584b3bc11345ca
Damien Cauquil <dcauquil@quarkslab.com>
2022-12-19 17:00:10 +0100

    Added device profile generation and emulation commands.

--- whad/ble/cli/utility/commands/__init__.py
+++ whad/ble/cli/utility/commands/__init__.py
@@ -6,3 +6,5 @@ from .scan import scan_handler
 from .shell import interactive_handler
 from .read import read_handler
 from .write import write_handler, writecmd_handler
+from .profile import profile_handler
+from .emulate import emulate_handler
