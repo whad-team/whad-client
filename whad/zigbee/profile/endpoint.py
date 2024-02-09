228593e15d157284bb84cf317cf5b06e48e94ea1
RCayre <rcayre@laas.fr>
2024-02-09 23:07:59 +0100

    Refactoring the zigbee profile

---
+++ whad/zigbee/profile/endpoint.py
@@ -0,0 +1,122 @@
+from whad.zigbee.stack.apl.zcl import ZCLClientCluster
+from whad.zigbee.stack.apl.zcl.clusters import *
+"""
+This module defines classes and exceptions related to ZigBee node's Endpoints.
+"""
+class ClusterNotAvailable(Exception):
+    """
+    Exception raised when a cluster is not available.
+    """
+    pass
+
+class Endpoint:
+    def __init__(self, number, node):
+        self.__number = number
+        self.__node = node
+        self.__descriptor = None
+
+    @property
+    def descriptor(self):
+        """
+        Property returning the descriptor associated to this endpoint.
+        """
+
+        if (
+            self.__descriptor is None and
+            self.__node.network.is_associated() and
+            self.__node.network.is_authorized()
+        ):
+            self.__descriptor = self.stack.apl.get_application_by_name("zdo").device_and_service_discovery.get_simple_descriptor(
+                self.__node.address,
+                self.__number
+            )
+        return self.__descriptor
+
+    @property
+    def stack(self):
+        """
+        Property returning the stack instance linked to this endpoint.
+        """
+        if self.__node is None:
+            return None
+        return self.__node.stack
+
+    @property
+    def node(self):
+        """
+        Property returning of this endpoint.
+        """
+        return self.__node
+
+
+    @property
+    def profile_id(self):
+        """
+        Property returning the profile ID of this endpoint.
+        """
+        return self.descriptor.profile_identifier
+
+    @property
+    def device_id(self):
+        """
+        Property indicating the device ID of this endpoint.
+        """
+        return self.descriptor.device_identifier
+
+    @property
+    def input_clusters(self):
+        """
+        Property returning the input clusters of this endpoint.
+        """
+        return self.descriptor.input_clusters
+
+    @property
+    def output_clusters(self):
+        """
+        Property returning the output clusters of this endpoint.
+        """
+        return self.descriptor.output_clusters
+
+    def attach_to_input_cluster(self, cluster_id, application=None):
+        """
+        Attach the endpoint to an input cluster in the stack, according
+        to the cluster id.
+        """
+        # Check if the cluster number is in the endpoint's input cluster list
+        if cluster_id not in self.descriptor.input_clusters:
+            raise ClusterNotAvailable
+
+        # Select the first application matching the profile
+        selected_application = None
+        for application in self.stack.apl.get_applications():
+            if application.profile_id == self.profile_id:
+                selected_application = application
+                break
+
+        # Check if a matching client cluster exists in our application
+        selected_cluster = None
+        for cluster in selected_application.output_clusters:
+            if cluster.cluster_id == cluster_id:
+                selected_cluster = cluster
+                break
+
+        if selected_cluster is None:
+            # Only works with ZCL, we should adapt it for other clusters
+            for candidate_cluster_class in ZCLClientCluster.child_clusters():
+                if candidate_cluster_class().cluster_id == cluster_id:
+                    selected_cluster = candidate_cluster_class()
+                    break
+
+        if selected_cluster is not None:
+            selected_application.add_output_cluster(selected_cluster)
+            selected_cluster.connect(self.device.address, self.number)
+
+        return selected_cluster
+
+
+    @property
+    def number(self):
+        return self.__number
+
+    def __repr__(self):
+        return "Endpoint(#%d, input_clusters=%s, output_clusters=%s)" % (self.number, str(self.input_clusters), str(self.output_clusters))
---
+++ whad/zigbee/profile/network.py
@@ -0,0 +1,154 @@
+from whad.zigbee.stack.nwk.constants import ZigbeeDeviceType
+from whad.zigbee.profile.nodes import CoordinatorNode, EndDeviceNode, RouterNode
+from time import sleep
+
+class JoiningForbidden(Exception):
+    """
+    Exception raised when joining is forbidden on the network.
+    """
+    pass
+
+class NotAssociated(Exception):
+    """
+    Exception raised when we are not associated in this network.
+    """
+    pass
+
+class NotAuthorized(Exception):
+    """
+    Exception raised when we are not authorized in this network.
+    """
+    pass
+
+class Network:
+    """
+    Class representing a ZigBee network.
+    """
+    def __init__(self, nwkObject=None, stack = None):
+        # reference to the corresponding network representation
+        # in the stack, we are defining an helper class here.
+        self.__nwk_object = nwkObject
+
+        # reference to the stack
+        self.__stack = stack
+
+        # only one coordinator, but possibly multiple routers & end devices
+        self.coordinator = None
+        self.routers = []
+        self.end_devices = []
+
+
+    @property
+    def nodes(self):
+        """
+        Returns all the nodes present in this network.
+        """
+        return (
+            [self.coordinator] if self.coordinator is not None else []
+        ) + self.routers + self.end_devices
+
+    @property
+    def stack(self):
+        """
+        Returns the associated instance of the stack.
+        """
+        return self.__stack
+
+    @property
+    def pan_id(self):
+        """
+        Returns the network Pan ID.
+        """
+        return self.__nwk_object.dot15d4_pan_network.coord_pan_id
+
+    @property
+    def extended_pan_id(self):
+        """
+        Returns the network extended Pan ID.
+        """
+        return self.__nwk_object.extended_pan_id
+
+    @property
+    def channel(self):
+        """
+        Returns the channel used by the network.
+        """
+        return self.__nwk_object.channel
+
+    def discover(self):
+        """
+        Discover all the nodes present in the network.
+        """
+        if not self.is_associated():
+            raise NotAssociated
+        if not self.is_authorized():
+            raise NotAuthorized
+        devices = self.stack.get_layer('apl').get_application_by_name("zdo").device_and_service_discovery.discover_devices()
+        return self.devices
+
+    def join(self):
+        """
+        Join the network (if permitted).
+
+        This method is blocking and wait until we are both associated AND authorized on the network.
+        """
+        if self.is_joining_permitted():
+            join_success = self.stack.get_layer('apl').get_application_by_name("zdo").network_manager.join(self)
+            if join_success:
+                while not self.is_authorized():
+                    sleep(0.1)
+                return True
+            return False
+        else:
+            raise JoiningForbidden
+
+    def is_joining_permitted(self):
+        """
+        Indicates if joining the network is permitted.
+        """
+        return self.__nwk_object.joining_permit
+
+    def is_associated(self):
+        """
+        Indicates if we are associated with the network.
+        """
+        return self.stack.get_layer('nwk').database.get("nwkExtendedPANID") == self.extended_pan_id
+
+    def is_authorized(self):
+        """
+        Indicates if we are authorized on the network.
+        """
+        return self.stack.get_layer('apl').get_application_by_name("zdo").network_manager.authorized
+
+    @property
+    def network_key(self):
+        """
+        Returns the network key associated with this network (if any).
+        """
+        nwk_material = self.stack.get_layer('nwk').database.get("nwkSecurityMaterialSet")
+        if len(nwk_material) > 0:
+            return nwk_material[0].key
+        else:
+            return None
+
+    def leave(self):
+        """
+        Initiates a leave operation on this network.
+        """
+        return self.stack.get_layer('apl').get_application_by_name("zdo").network_manager.leave()
+
+    def __eq__(self, other):
+        return self.extended_pan_id == other.extended_pan_id
+
+    def __repr__(self):
+        return (
+            "Network(" +
+            "pan_id=" + hex(self.pan_id) + ", " +
+            "extended_pan_id=" + hex(self.extended_pan_id) + ", " +
+            "channel="+str(self.channel) + ", " +
+            "joining="+ ("allowed" if self.is_joining_permitted() else "forbidden") + ", " +
+            "associated="+ ("yes" if self.is_associated() else "no") + ", "
+            "authorized="+ ("yes" if self.is_authorized() else "no") +
+
+            ")"
+        )
