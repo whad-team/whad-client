11e86e3f28e9445e632ea3397249d05e0940d046
RCayre <rcayre@laas.fr>
2024-02-27 15:00:38 +0100

    Fixing transport key primitive

--- whad/zigbee/crypto.py
+++ whad/zigbee/crypto.py
@@ -158,8 +158,9 @@ class CryptoManager:
         self.auth = self.generateAuth(packet)
 
         # Extract plaintext
+        print(packet.data)
         plaintext = packet.data[:-self.M] if len(packet.mic) == 0 and self.patched else packet.data
-
+        print(plaintext.hex())
         # Encrypt and generate MIC
         cipher = AES.new(self.key, AES.MODE_CCM, nonce=self.nonce, mac_len=self.M)
         cipher.update(self.auth)
