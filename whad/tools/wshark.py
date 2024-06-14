003dfbe9cee81a4c9a36d44c31f9101d7d216b1a
RCayre <rcayre@laas.fr>
2024-06-14 17:59:32 +0200

    Starting implem of wshark

--- whad/tools/wshark.py
+++ whad/tools/wshark.py
@@ -12,7 +12,7 @@ from scapy.all import *
 from whad.device.unix import UnixSocketProxy, UnixSocketCallbacksConnector
 from whad.exceptions import WhadDeviceNotFound, WhadDeviceNotReady
 from whad.cli.ui import error, warning, success, info, display_event, display_packet
-
+from whad.common.monitors import WiresharkMonitor
 import logging
 import time
 import sys
@@ -20,76 +20,28 @@ import sys
 logger = logging.getLogger(__name__)
 #logging.basicConfig(level=logging.DEBUG)
 
-class WhadExtractApp(CommandLineApp):
+class WhadWiresharkApp(CommandLineApp):
 
     def __init__(self):
         """Application uses an interface and has commands.
         """
         super().__init__(
-            description='WHAD filter tool',
+            description='WHAD wireshark',
             interface=True,
             commands=False,
             input=CommandLineApp.INPUT_WHAD,
-            output=CommandLineApp.OUTPUT_STANDARD
+            output=CommandLineApp.OUTPUT_WHAD
         )
-
-        self.add_argument(
-            'extractor',
-            help='extractor to evaluate',
-            nargs='*'
-        )
-
-        self.add_argument(
-            '-d',
-            '--delimiter',
-            dest='delimiter',
-            default=" ",
-            help='delimiter between extractor'
-        )
-
-    def build_extractors(self):
-        extractor_template = "lambda p : %s"
-        extractors = []
-        for extractor in self.args.extractor:
-            if "packet." in extractor:
-                extractor.replace("packet.", "p.")
-            elif "pkt." in extractor:
-                extractor.replace("pkt.", "p.")
-
-            extractors.append(eval(extractor_template % extractor))
-
-        return extractors
 
     def on_rx_packet(self, pkt):
-        extractors = self.build_extractors()
-        output = []
-
-        try:
-            for extractor in extractors:
-
-                output.append(str(extractor(pkt)))
-
-            print(self.args.delimiter.join(output))
-            sys.stdout.flush()
-            return pkt
-        except:
-            return pkt
+        return pkt
 
     def on_tx_packet(self, pkt):
-        extractors = self.build_extractors()
-        output = []
-        try:
-            for extractor in extractors:
-                output.append(str(extractor(pkt)))
-
-            print(self.args.delimiter.join(output))
-            sys.stdout.flush()
-            return pkt
-        except:
-            return pkt
+        return pkt
 
     def run(self):
         # Launch pre-run tasks
+        monitor = None
         self.pre_run()
         try:
             if self.is_piped_interface():
@@ -107,24 +59,31 @@ class WhadExtractApp(CommandLineApp):
                     "on_tx_packet_cb" : self.on_tx_packet,
                     "on_rx_packet_cb" : self.on_rx_packet,
                 })
+                monitor = WiresharkMonitor()
+                monitor.attach(interface)
+                monitor.start()
+
                 interface.open()
+
 
                 proxy = UnixSocketProxy(
                     interface,
                     params=parameters,
                     connector=UnixSocketCallbacksConnector
                 )
-                #proxy.start()
-                #proxy.join()
+                proxy.start()
+                proxy.join()
 
                 while True:
                     time.sleep(1)
 
         except KeyboardInterrupt:
             # Launch post-run tasks
+            if monitor is not None:
+                monitor.stop()
             self.post_run()
 
 
-def wextract_main():
-    app = WhadExtractApp()
+def wshark_main():
+    app = WhadWiresharkApp()
     app.run()
