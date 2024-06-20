a4f587a5ab2acc84edcf7bbe8b2745656e858665
Damien Cauquil <virtualabs@gmail.com>
2024-06-20 17:33:00 +0200

    Add timeout support to sniff() method.

--- whad/unifying/connector/sniffer.py
+++ whad/unifying/connector/sniffer.py
@@ -1,3 +1,8 @@
+from time import time
+from typing import Generator, List
+
+from scapy.packet import Packet
+
 from whad.unifying.connector import Unifying
 from whad.unifying.sniffing import SnifferConfiguration, KeyExtractedEvent
 from whad.unifying.crypto import LogitechUnifyingDecryptor, LogitechUnifyingKeyDerivation
@@ -5,6 +10,9 @@ from whad.exceptions import UnsupportedCapability
 from whad.helpers import message_filter, is_message_type
 from whad.common.sniffing import EventsManager
 from whad.scapy.layers.unifying import Logitech_Encrypted_Keystroke_Payload
+
+from whad.hub.message import AbstractPacket
+from whad.hub.unifying import RawPduReceived, PduReceived
 import logging
 
 logger = logging.getLogger(__name__)
@@ -14,6 +22,8 @@ class Sniffer(Unifying, EventsManager):
     Logitech Unifying Sniffer interface for compatible WHAD device.
     """
     def __init__(self, device):
+        """Sniffer initialization.
+        """
         Unifying.__init__(self, device)
         EventsManager.__init__(self)
 
@@ -28,6 +38,11 @@ class Sniffer(Unifying, EventsManager):
 
 
     def _enable_sniffing(self):
+        """Enable sniffing.
+
+        This method takes the current sniffing configuration and configure the
+        hardware accordingly.
+        """
         for key in self.__configuration.keys:
             self.__decryptor.add_key(key)
         if self.__configuration.scanning:
@@ -45,10 +60,14 @@ class Sniffer(Unifying, EventsManager):
 
     @property
     def configuration(self):
+        """Current sniffing configuration accessor.
+        """
         return self.__configuration
 
     @configuration.setter
     def configuration(self, new_configuration):
+        """Sniffing configuration setter.
+        """
         self.stop()
         self.__configuration = new_configuration
         self._enable_sniffing()
@@ -58,77 +77,123 @@ class Sniffer(Unifying, EventsManager):
         return self.__configuration.channel
 
     @channel.setter
-    def channel(self, channel=11):
+    def channel(self, channel: int = 11):
+        """Sniffing channel setter.
+
+        :param channel: Channel to use
+        :type channel: int
+        """
         self.stop()
         self.__configuration.channel = channel
         self._enable_sniffing()
 
-    def add_key(self, key):
+    def add_key(self, key: bytes):
+        """Add encryption key to the curret sniffing configuration.
+
+        :param key: Key to add
+        :type key: bytes
+        """
         self.stop()
         self.__configuration.keys.append(key)
         self._enable_sniffing()
 
     def clear_keys(self):
+        """Remove all registered keys from current sniffing configuration.
+        """
         self.stop()
         self.__configuration.keys = []
         self._enable_sniffing()
 
     @property
     def decrypt(self):
+        """Current sniffing decryption flag accessor.
+        """
         return self.__configuration.decrypt
 
     @decrypt.setter
-    def decrypt(self, decrypt):
+    def decrypt(self, decrypt: bool):
+        """Enable or disable decryption in current sniffing configuration.
+        """
         self.stop()
         self.__configuration.decrypt = decrypt
         self._enable_sniffing()
 
     @property
     def address(self):
+        """Target address accessor.
+        """
         return self.__configuration.address
 
     @address.setter
-    def address(self, address):
+    def address(self, address: str):
+        """Target address setter.
+
+        :param address: target address
+        :type address: str
+        """
         self.stop()
         self.__configuration.address = address
         self._enable_sniffing()
 
     @property
     def scanning(self):
+        """Current sniffing configuration scanning flag accessor.
+        """
         return self.__configuration.scanning
 
     @scanning.setter
-    def scanning(self, scanning):
+    def scanning(self, scanning: bool):
+        """Enable or disable scanning.
+
+        :param scanning: Enable scanning if set to True, disable it if False
+        :type scanning: bool
+        """
         self.stop()
         self.__configuration.scanning = scanning
         self._enable_sniffing()
 
-    def available_actions(self, filter=None):
+    def available_actions(self, filter=None) -> List:
+        """Identify available actions.
+        """
         actions = []
         return [action for action in actions if filter is None or isinstance(action, filter)]
 
-    def sniff(self):
+    def sniff(self, timeout: float = None) -> Generator[Packet, None, None]:
+        """Sniff Logitech Unifying packets
+
+        :param timeout: Number of seconds after which sniffing stops, uninterrupted if set to None
+        :type timeout: float
+        """
+        # Determine message type
+        if self.support_raw_pdu():
+            message_type = RawPduReceived
+        else:
+            message_type = PduReceived
+
+        # Sniff packets
+        start = time()
         while True:
-            if self.support_raw_pdu():
-                message_type = "raw_pdu"
-            else:
-                message_type = "pdu"
 
-            message = self.wait_for_message(filter=message_filter('unifying', message_type))
-            packet = self.translator.from_message(message.unifying, message_type)
+            # Exit if timeout is set and reached
+            if timeout is not None and (time() - start >= timeout):
+                break
 
-            if self.__configuration.pairing:
-                self.__key_derivation.process_packet(packet)
-                if self.__key_derivation.key is not None:
-                    logger.info("[i] New key extracted: ", self.__key_derivation.key.hex())
-                    self.trigger_event(KeyExtractedEvent(self.__key_derivation.key))
-                    self.add_key(self.__key_derivation.key)
-                    self.__key_derivation.reset()
+            message = self.wait_for_message(filter=message_filter(message_type), timeout=.1)
+            if issubclass(message, AbstractPacket):
+                packet = message.to_packet()
 
-            if Logitech_Encrypted_Keystroke_Payload in packet and self.__configuration.decrypt:
-                decrypted, success = self.__decryptor.attempt_to_decrypt(packet)
+                if self.__configuration.pairing:
+                    self.__key_derivation.process_packet(packet)
+                    if self.__key_derivation.key is not None:
+                        logger.info("[i] New key extracted: ", self.__key_derivation.key.hex())
+                        self.trigger_event(KeyExtractedEvent(self.__key_derivation.key))
+                        self.add_key(self.__key_derivation.key)
+                        self.__key_derivation.reset()
 
-                if success:
-                    packet.decrypted = decrypted
-            self.monitor_packet_rx(packet)
-            yield packet
+                if Logitech_Encrypted_Keystroke_Payload in packet and self.__configuration.decrypt:
+                    decrypted, success = self.__decryptor.attempt_to_decrypt(packet)
+
+                    if success:
+                        packet.decrypted = decrypted
+                self.monitor_packet_rx(packet)
+                yield packet
