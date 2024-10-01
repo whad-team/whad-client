3908a5633cafec5d6845a3f1c12ee2947b815f12
Damien Cauquil <virtualabs@gmail.com>
2024-10-01 17:11:20 +0200

    Refactored the EnhancedShockBurst scanning features and fixed some minors typos/unused imports.

--- whad/esb/__init__.py
+++ whad/esb/__init__.py
@@ -1,3 +1,10 @@
+"""WHAD Enhanced ShockBurst protocol
+
+This module exposes a dedicated connector for the ESB
+protocol, as well as a sniffer, a scanner, a receiver (PRX)
+and a transmitter (PTX) connectors.
+"""
+
 from whad.esb.connector import ESB, Sniffer, PRX, PTX, Scanner
 from whad.esb.utils.phy import PHYS
 __all__ = [
