30a6e2b4a79f5112d73cca3e2985b8458385c7f7
Damien Cauquil <dcauquil@quarkslab.com>
2025-05-30 11:19:52 +0200

    Improved winject to get good performances and avoid lags that sometimes caused an error when chained with wplay in flush mode.

--- whad/tools/winject.py
+++ whad/tools/winject.py
@@ -93,7 +93,11 @@ class WhadInjectApp(CommandLineApp):
         self._input_queue = Queue()
         self._repeat_queue = Queue()
 
-        # Don't build yet if piped
+        # Load implemented injectors (introspection)
+        self.environment = None
+
+        # Build subparsers if stdin is not piped, when piped we will do
+        # it in the pre_run() method.
         if not self.is_stdin_piped():
             self.subparsers = self.add_subparsers(
                 required=True,
@@ -101,7 +105,7 @@ class WhadInjectApp(CommandLineApp):
                 parser_class=WhadDomainSubParser,
                 help='Domain in use'
             )
-
+            self.environment = list_implemented_injectors()
             self.build_subparsers(self.subparsers)
         else:
             self.subparsers = None
@@ -116,7 +120,6 @@ class WhadInjectApp(CommandLineApp):
             domain["parameters"] = get_injector_parameters(
                 domain["configuration_class"]
             )
-
 
             domain["subparser"] = subparsers.add_parser(
                 domain_name,
@@ -248,14 +251,14 @@ class WhadInjectApp(CommandLineApp):
             self.print_help()
             sys.exit(1)
 
-
         error_func = self.error
         if self.subparsers is None:
             self.error  = lambda m : None
 
         # Load implemented injectors *before* calling super().__init__()
-        # drastically improves performances.
-        self.environment = list_implemented_injectors()      
+        # drastically improves performances... and I have no clue why o_o
+        if self.environment is None:
+            self.environment = list_implemented_injectors()
 
         # Call CLI app pre-run: it will initialize our source interface
         super().pre_run()
@@ -279,13 +282,17 @@ class WhadInjectApp(CommandLineApp):
                 help='Domain in use'
             )
 
+            # We build subparsers.
             self.build_subparsers(self.subparsers)
 
+            # And we inject the domain parameter retrieved from the parameters
+            # given by the input tool through the Unix socket URI.
             if domain not in start_argv and domain not in end_argv:
                 sys.argv = start_argv + [domain] + end_argv
             else:
                 sys.argv = start_argv + end_argv
 
+            # Parse winject arguments and propagate into the `self.args` namespace.
             for k, v in self.parse_args().__dict__.items():
                 setattr(self.args, k, v)
 
@@ -293,6 +300,7 @@ class WhadInjectApp(CommandLineApp):
         if not self.args.nocolor:
             conf.color_theme = BrightTheme()
 
+        # Generate packets if provided through commandline
         self.generate_packets()
 
 
@@ -316,7 +324,7 @@ class WhadInjectApp(CommandLineApp):
         injector = None
         # Launch pre-run tasks
         self.pre_run()
-        
+
         try:
             # We need to have an interface specified
             if self.interface is not None:
