9e05481ab2efde7b34ef249fb927eb02526e8775
Damien Cauquil <dcauquil@quarkslab.com>
2025-06-23 22:26:07 +0200

    Added first draft implementation of WHAD mnock devices and tried to use it for BLE scanner unit testing.

---
+++ whad/device/mock/__init__.py
@@ -0,0 +1,15 @@
+"""WHAD mock device
+
+This module provides various classes and helpers to implement mock devices
+for WHAD:
+
+- WhadDevice
+"""
+
+from .base import MockDevice
+from .replay import ReplayMock
+
+__all__ = [
+    "MockDevice",
+    "ReplayMock",
+]
