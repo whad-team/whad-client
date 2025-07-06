bb6e02751abdb96d4886c06c1b6439acae405ed8
erdnaxe <erdnaxe@crans.org>
2025-07-06 08:34:30 +0200

    whad/hub/ble/bdaddr: modernize

--- whad/hub/ble/bdaddr.py
+++ whad/hub/ble/bdaddr.py
@@ -1,11 +1,11 @@
-import re, json
-from binascii import hexlify, unhexlify
+import json
+import re
+
 
 class InvalidBDAddressException(Exception):
     """Invalid BD address used
     """
-    def __init__(self):
-        super().__init__()
+
 
 class BDAddress(object):
     """This class represents a Bluetooth Device address.
@@ -14,7 +14,7 @@ class BDAddress(object):
     PUBLIC = 0x00
     RANDOM = 0x01
 
-    def __init__(self, address, random=False, addr_type=None):
+    def __init__(self, address, random: bool = False, addr_type=None):
         """Initialize BD address
 
         By default, BD address is public unless `random` is set to True.
@@ -31,10 +31,10 @@ class BDAddress(object):
             self.__type = BDAddress.PUBLIC
 
         if isinstance(address, str):
-            if re.match(r'^([0-9a-fA-F]{2}\:){5}[0-9a-fA-F]{2}$', address) is not None:
-                self.__value = unhexlify(address.replace(':',''))[::-1]
-            elif re.match('[0-9a-fA-F]{12}$', address) is not None:
-                self.__value = unhexlify(address)[::-1]
+            if re.match(r"^([0-9a-fA-F]{2}\:){5}[0-9a-fA-F]{2}$", address) is not None:
+                self.__value = bytes.fromhex(address.replace(":", ""))[::-1]
+            elif re.match("[0-9a-fA-F]{12}$", address) is not None:
+                self.__value = bytes.fromhex(address)[::-1]
             else:
                 raise InvalidBDAddressException
         elif isinstance(address, bytes) and len(address) == 6:
@@ -46,57 +46,54 @@ class BDAddress(object):
         return (self.__value == other.value) and (self.__type == other.type)
 
     def __str__(self):
-        return ':'.join(['%02x' % b for b in self.__value[::-1]])
+        return ":".join(f"{b:02x}" for b in self.__value[::-1])
 
     def __repr__(self):
-        return 'BDAddress(%s)' % str(self)
+        return f"BDAddress({str(self)})"
 
-    def export_json(self):
+    def export_json(self) -> str:
         return json.dumps(str(self))
 
     @property
-    def type(self):
+    def type(self) -> int:
         return self.__type
 
     @property
-    def value(self):
+    def value(self) -> bytes:
         return self.__value
 
-    def is_public(self):
+    def is_public(self) -> bool:
         """Determine if address is public.
 
         :rtype: bool
         :return: True if BD address is public, False otherwise.
         """
-        return (self.__type == BDAddress.PUBLIC)
+        return self.__type == BDAddress.PUBLIC
 
-    def is_random(self):
+    def is_random(self) -> bool:
         """Determine if address is random.
 
         :rtype: bool
         :return: True if BD address is random, False otherwise.
         """
-        return (self.__type == BDAddress.RANDOM)
+        return self.__type == BDAddress.RANDOM
 
     @staticmethod
-    def from_bytes(bd_addr_bytes, addr_type=None):
-        """Convert a 6-byte array into a valid BD address.
+    def from_bytes(bd_addr_bytes: bytes, addr_type=None) -> "BDAddress":
+        """Convert a 6-byte array into a Bluetooth address.
 
-        :param bytes bd_addr_bytes: Bluetooth Device address as a bytearray.
-        :param addr_type: Bluetooth Device Address type
+        :param bytes bd_addr_bytes: Bluetooth Device address as bytes.
+        :param addr_type: Bluetooth Device Address type, PUBLIC or RANDOM
         :type addr_type: int, optional
         :rtype: BDAddress
         :return: An instance of BDAddress representing the corresponding BD address.
         """
         if len(bd_addr_bytes) == 6:
-            hex_address = hexlify(bd_addr_bytes[::-1])
-            address = b':'.join([hex_address[i*2:(i+1)*2] for i in range(int(len(hex_address)/2))])
-            return BDAddress(address.decode('utf-8'), addr_type=addr_type)
+            return BDAddress(bd_addr_bytes, addr_type=addr_type)
         else:
             raise InvalidBDAddressException
 
     @staticmethod
-
     def check(bd_addr: str) -> bool:
         """Check if a BD address is valid
 
@@ -105,4 +102,4 @@ class BDAddress(object):
         :return:    ``True`` if BD address is valid, ``False`` otherwise
         :rtype:     bool
         """
-        return re.match('^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$',bd_addr)
+        return bool(re.match("^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$", bd_addr))
