0aaf283688ed46752063490a6e30e9ef940f7d0d
Damien Cauquil <dcauquil@quarkslab.com>
2025-07-08 17:49:26 +0200

    Fixed lots of bugs related to L2CAP/ATT/GATT packet parsing/crafting and mock device message passing.

--- whad/ble/mock/stack/read.py
+++ whad/ble/mock/stack/read.py
@@ -4,7 +4,10 @@
 The Read procedure is initiated by a Read request, and based on the required
 attribute handle returns the attribute value (success) or an error (failure).
 """
-from scapy.layers.bluetooth import ATT_Read_Request, ATT_Read_Response
+from typing import List
+
+from scapy.packet import Packet
+from scapy.layers.bluetooth import ATT_Hdr, ATT_Read_Request, ATT_Read_Response
 
 from .attribute import find_attr_by_handle
 from .procedure import Procedure
@@ -21,20 +24,20 @@ class ReadProcedure(Procedure):
         """Determine if the procedure is triggered."""
         return ATT_Read_Request in request
 
-    def process_request(self, request):
+    def process_request(self, request) -> List[Packet]:
         """React only on ReadRequest."""
         request = request.getlayer(ATT_Read_Request)
 
         # We have a read request, look for the requested attribute
         try:
             # Query attributes
-            attrib = find_attr_by_handle(self.attributes, request.handle)
+            attrib = find_attr_by_handle(self.attributes, request.gatt_handle)
 
             # Attribute is found, return a ReadResponse and mark procedure
             # as done
             self.set_state(Procedure.STATE_DONE)
-            return ATT_Read_Response(attrib.value)
+            return [ATT_Hdr()/ATT_Read_Response(attrib.value)]
         except IndexError:
             self.set_state(Procedure.STATE_DONE)
-            return ATT_Error_Response(0x0A, request.handle, 0x01)
+            return [ATT_Hdr()/ATT_Error_Response(0x0A, request.handle, 0x01)]
 
