44c12618d7d853c811ab0f6c0ff457a8aa9e6767
Damien Cauquil <virtualabs@gmail.com>
2025-08-08 22:47:41 +0200

    Add unit tests for GATT client ReadBlob procedure used in Peripheral.

--- whad/ble/mock/peripheral.py
+++ whad/ble/mock/peripheral.py
@@ -193,6 +193,21 @@ class PeripheralMock(MockDevice):
         # Wait for the client procedure to terminate
         return self.__client.wait_procedure(timeout=1.0)
 
+    def read_blob(self, handle: int, offset: int):
+        """Emulate a ReadBlob procedure initiated by a remote central."""
+        # Start a Read procedure from an emulated central device.
+        self.__client.read_blob(handle, offset)
+
+        # Retrieve waiting packets from L2CAP layer and convert them to messages,
+        # and send them to the attached connector (if any)
+        messages = self.to_messages(self.__l2cap.get_pdus())
+        for msg in messages:
+            self.put_message(msg)
+
+        # Wait for the client procedure to terminate
+        return self.__client.wait_procedure(timeout=1.0)
+
+
     def find_information(self, start_handle: int, end_handle: int):
         """Emulate a FindInformation procedure initiated by a remote central.
 
