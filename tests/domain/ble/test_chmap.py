7b6c9b0d0d57800337ddb84057e0c8df72c41d6b
Damien Cauquil <dcauquil@quarkslab.com>
2025-09-26 11:56:57 +0200

    Added features to ChannelMap class to ease filtering and get number of active channels.

--- tests/domain/ble/test_chmap.py
+++ tests/domain/ble/test_chmap.py
@@ -20,7 +20,7 @@ class TestChannelMap(object):
         chanmap = ChannelMap()
         chanmap.add(20)
         assert chanmap.value == b'\x00\x00\x10\x00\x00'
-    
+
     def test_chanmap_remove(self):
         """Test channel map remove() method
         """
@@ -33,7 +33,7 @@ class TestChannelMap(object):
         assert chanmap.has(12) == True
         assert chanmap.has(13) == True
         assert chanmap.has(14)
-    
+
     def test_chanmap_invalid_chan(self):
         chanmap = ChannelMap(channels=[12, 13, 14])
         with pytest.raises(ValueError) as e_info:
@@ -46,3 +46,22 @@ class TestChannelMap(object):
     def test_chanmap_from_int(self):
         chanmap = ChannelMap.from_int(0x1faabbccdd)
         assert chanmap.value == b"\xdd\xcc\xbb\xaa\x1f"
+
+    def test_chanmap_filter(self):
+        chanmap = ChannelMap([1,2,3,4,5, 20, 21, 22])
+        chanmap.filter(lambda x: x>=20)
+        assert chanmap == ChannelMap([20, 21, 22])
+
+    def test_chanmap_compare(self):
+        a = ChannelMap([1,2,3])
+        b = ChannelMap([1,2,3])
+        assert a==b
+
+    def test_chanmap_compare_different(self):
+        a = ChannelMap([1,2,3])
+        b = ChannelMap([5,6,7])
+        assert a != b
+
+    def test_chanmap_len(self):
+        a = ChannelMap([1,2,3,4,5])
+        assert len(a) == 5
