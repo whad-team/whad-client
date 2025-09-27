5e091df7b600f84de89d5725459824a4a9d9e43f
Damien Cauquil <dcauquil@quarkslab.com>
2025-09-27 11:37:50 +0200

    Added unit/functional tests for BLE Advertiser connector

--- whad/ble/mock/__init__.py
+++ whad/ble/mock/__init__.py
@@ -3,9 +3,11 @@
 from .device import EmulatedDevice
 from .scan import DeviceScan
 from .central import CentralMock
+from .advertiser import AdvertiserMock
 
 __all__ = [
     "CentralMock",
     "DeviceScan",
     "EmulatedDevice",
+    "AdvertiserMock",
 ]
