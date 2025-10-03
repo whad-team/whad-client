3510c5b3792901c7f9ac4bc10392762644ebd307
Damien Cauquil <dcauquil@quarkslab.com>
2025-10-03 10:08:33 +0200

    Updated documentation to fix multiple errors.

--- whad/ble/__init__.py
+++ whad/ble/__init__.py
@@ -6,22 +6,33 @@ import re
 from whad.ble.stack.gatt import GattClient, GattServer
 from whad.common.triggers import ManualTrigger, ConnectionEventTrigger, ReceptionTrigger
 from whad.hub.ble.bdaddr import BDAddress
+from whad.hub.ble.chanmap import ChannelMap
 from whad.ble.profile import GenericProfile
-from whad.ble.profile.advdata import AdvDataFieldList, AdvFlagsField, AdvDataField, AdvCompleteLocalName, \
-    AdvManufacturerSpecificData, AdvShortenedLocalName, AdvTxPowerLevel, AdvDataFieldListOverflow, AdvDataError
+from whad.ble.profile.advdata import (
+    AdvDataFieldList, AdvFlagsField, AdvDataField, AdvCompleteLocalName,
+    AdvManufacturerSpecificData, AdvShortenedLocalName, AdvTxPowerLevel,
+    AdvDataFieldListOverflow, AdvURI, AdvLeRole, AdvAppearance, AdvUuid128List,
+    AdvUuid16List, AdvDataError
+)
 from whad.ble.connector.base import BLE
-from whad.ble.connector import Central, Peripheral, Sniffer, Hijacker, Injector, Scanner, PeripheralClient
+from whad.ble.connector import (
+    Central, Peripheral, Sniffer, Hijacker, Injector, Scanner, PeripheralClient, Scanner
+)
 from whad.ble.utils.phy import PHYS
 
 def is_bdaddr_valid(bd_addr):
     return re.match('^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$',bd_addr)
 
 __all__ = [
+    # Basic types for BLE
     'BLE',
+    'ChannelMap',
     'GattClient',
     'GattServer',
     'BDAddress',
     'GenericProfile',
+
+    # Advertising data classes
     'AdvDataFieldList',
     'AdvFlagsField',
     'AdvDataField',
@@ -30,7 +41,14 @@ __all__ = [
     'AdvShortenedLocalName',
     'AdvTxPowerLevel',
     'AdvDataFieldListOverflow',
+    'AdvURI',
+    'AdvLeRole',
+    'AdvAppearance',
+    'AdvUuid128List',
+    'AdvUuid16List',
     'AdvDataError',
+
+    # Connectors
     'Central',
     'Peripheral',
     'PeripheralClient',
@@ -38,6 +56,8 @@ __all__ = [
     'Hijacker',
     'Injector',
     'Scanner',
+
+    # Misc.
     'PHYS',
     'ConnectionEventTrigger',
     'ManualTrigger',
