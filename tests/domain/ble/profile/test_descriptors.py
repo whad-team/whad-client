24180cf8b2cce2d373b34c42dc187ba3f71c4746
Damien Cauquil <dcauquil@quarkslab.com>
2025-12-15 07:50:02 +0100

    Updated unit tests related to BLE profile following the recent rework.

--- tests/domain/ble/profile/test_descriptors.py
+++ tests/domain/ble/profile/test_descriptors.py
@@ -1,27 +1,21 @@
 """Test WHAD BLE GATT profile descriptors.
 """
 import pytest
-from whad.ble.profile import CharacteristicDescriptor
-
-class DummyClass:
-    """Dummy class for descriptors.
-    """
-    pass
+from whad.ble.profile import CharacteristicDescriptor, UUID
 
 @pytest.fixture
 def gen_descriptor():
     """Generate a generic descriptor (fixture)
     """
 
-    return CharacteristicDescriptor(DummyClass)
-    
+    return CharacteristicDescriptor(UUID(0x1234))
 
 def test_charac_desc(gen_descriptor):
     """Test generic descriptor
     """
 
     assert(gen_descriptor.handle == 0)
-    assert(gen_descriptor.bleclass == DummyClass)
+    assert(gen_descriptor.uuid == UUID(0x1234))
 
 def test_charac_desc_update(gen_descriptor):
     """Test generic descriptor update
