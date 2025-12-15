489a5eca45c02864a50cd88e77e76fe5ee3300e8
Damien Cauquil <dcauquil@quarkslab.com>
2025-12-15 07:50:02 +0100

    Updated unit tests related to BLE profile following the recent rework.

--- tests/domain/ble/profile/test_charac.py
+++ tests/domain/ble/profile/test_charac.py
@@ -2,62 +2,61 @@
 """
 
 import pytest
-from whad.ble.profile import Characteristic, UUID, ReportReferenceDescriptor, PrimaryService
+from whad.ble.profile.characteristic import Characteristic, UUID, CharacteristicUserDescriptionDescriptor, ReportReferenceDescriptor
+from whad.ble.profile.service import PrimaryService
 
 def test_charac_simple_new():
     """Test characteristic instanciation without descriptors.
     """
-    charac = Characteristic(name="TestCharac", uuid=UUID(0x1234), value=b"foobar",
+    charac = Characteristic(uuid=UUID(0x1234), value=b"foobar",
                             permissions=["read", "write"], notify=False, indicate=False,
-                            Security=None)
-    assert(charac.name == "TestCharac")
+                            security=None)
     assert(charac.uuid == UUID(0x1234))
     assert(charac.value == b"foobar")
-    assert(charac.permissions == ["read", "write"])
-    assert(charac.must_notify == False)
-    assert(charac.must_indicate == False)
+    assert(charac.properties == (Characteristic.READ | Characteristic.WRITE))
+    assert(charac.must_notify() == False)
+    assert(charac.must_indicate() == False)
     assert(charac.security == [])
 
 def test_charac_desc_new():
     """Test characteristic instanciation with descriptors
     """
     dummy_desc = ReportReferenceDescriptor()
-    charac = Characteristic(name="TestCharac", uuid=UUID(0x1234), value=b"foobar",
+    charac = Characteristic(UUID(0x1234), value=b"foobar",
                             permissions=["read", "write"], notify=False, indicate=False,
-                            Security=None, dummy_desc=dummy_desc)
+                            security=None, dummy_desc=dummy_desc)
     desc_list = list(charac.descriptors())
     assert(dummy_desc in desc_list)
-    assert(desc_list[0] == dummy_desc)
-    
+    assert(desc_list[0] == dummy_desc) 
 
 def test_charac_required_handles():
     """Test if characteristic correctly computes the required handles.
     """
-    charac = Characteristic(name="TestCharac", uuid=UUID(0x1234), value=b"foobar",
+    charac = Characteristic(UUID(0x1234), value=b"foobar",
                             permissions=["read", "write"], notify=False, indicate=False,
-                            Security=None)
+                            security=None)
     assert(charac.get_required_handles() == 2)
 
-    charac_ = Characteristic(name="TestCharac", uuid=UUID(0x1234), value=b"foobar",
+    charac_ = Characteristic(UUID(0x1234), value=b"foobar",
                             permissions=["read", "write"], notify=False, indicate=True,
-                            Security=None)
+                            security=None)
     assert(charac_.get_required_handles() == 3)
 
 def test_charac_description():
     """Test characteristic description.
     """
-    charac = Characteristic(name="TestCharac", uuid=UUID(0x1234), value=b"foobar",
+    charac = Characteristic(UUID(0x1234), value=b"foobar",
                         permissions=["read", "write"], notify=False, indicate=False,
-                        Security=None, description="Foobar")
-    assert(charac.description == "Foobar")
+                        security=None, description="Foobar")
+    assert charac.get_descriptor(CharacteristicUserDescriptionDescriptor) is not None
 
 def test_charac_service_attach():
     """Test characteristic service setter.
     """
     service = PrimaryService(uuid=UUID(0x1800))
-    charac = Characteristic(name="TestCharac", uuid=UUID(0x1234), value=b"foobar",
+    charac = Characteristic(UUID(0x1234), value=b"foobar",
                         permissions=["read", "write"], notify=False, indicate=False,
-                        Security=None)
+                        security=None)
     charac.attach(service)
     assert(charac.service == service)
     assert(charac.service.uuid == service.uuid)
@@ -65,9 +64,9 @@ def test_charac_service_attach():
 def test_charac_handles():
     """Test characteristic handle manipulation.
     """
-    charac = Characteristic(name="TestCharac", uuid=UUID(0x1234), value=b"foobar",
+    charac = Characteristic(UUID(0x1234), value=b"foobar",
                         permissions=["read", "write"], notify=False, indicate=False,
-                        Security=None)
+                        security=None)
     assert(charac.handle == 0)
     charac.handle = 1
     assert(charac.handle == 1)
