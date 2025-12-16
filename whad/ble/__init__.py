5b1c402c491ac0e649d06d10b75e51fc87dd363f
Damien Cauquil <dcauquil@quarkslab.com>
2025-12-16 11:02:17 +0100

    Added `GapService` as an exported symbol.

--- whad/ble/__init__.py
+++ whad/ble/__init__.py
@@ -22,7 +22,7 @@ from whad.ble.profile.advdata import (
     AdvDataFieldListOverflow, AdvURI, AdvLeRole, AdvAppearance, AdvUuid128List,
     AdvUuid16List, AdvDataError
 )
-from whad.ble.profile.services import BatteryService, DeviceInformationService
+from whad.ble.profile.services import BatteryService, DeviceInformationService, GapService
 from whad.ble.connector.base import BLE
 from whad.ble.connector import (
     Central, Peripheral, Sniffer, Hijacker, Injector, Scanner, PeripheralClient, Scanner
@@ -47,6 +47,7 @@ __all__ = [
     'subscribed',
     'unsubscribed',
     'BatteryService',
+    'GapService',
     'DeviceInformationService',
     'Characteristic',
     'Descriptor',
