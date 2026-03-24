5310541b9afd65f4b8c48e43f08bd76512b58407
Damien Cauquil <dcauquil@quarkslab.com>
2026-03-24 07:53:17 +0100

    Version bump (1.2.14 to 1.2.15)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,7 @@ author = 'Damien Cauquil, Romain Cayre'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.2.14'
+release = '1.2.15'
 
 
 # -- General configuration ---------------------------------------------------
