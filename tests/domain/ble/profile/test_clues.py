e214a7c463bf3a81bb9594268f6c44f658ae2a0b
Damien Cauquil <dcauquil@quarkslab.com>
2026-06-01 10:12:16 +0200

    Migrated to latest version of CLUES database (structure has been modified).

--- tests/domain/ble/profile/test_clues.py
+++ tests/domain/ble/profile/test_clues.py
@@ -8,8 +8,8 @@ from whad.ble.utils.clues import CluesDb
 def test_clues_data():
     """Make sure CLUES data files have been fetched.
     """
-    assert os.path.exists("./whad/resources/clues/CLUES_data.json")
-    assert os.path.isfile("./whad/resources/clues/CLUES_data.json")
+    assert os.path.exists("./whad/resources/clues/data/CLUES_data_human_verified.json")
+    assert os.path.isfile("./whad/resources/clues/data/CLUES_data_human_verified.json")
     # load JSON
-    with open("./whad/resources/clues/CLUES_data.json", 'r', encoding="utf-8") as clues_json:
+    with open("./whad/resources/clues/data/CLUES_data_human_verified.json", 'r', encoding="utf-8") as clues_json:
         json.load(clues_json)
